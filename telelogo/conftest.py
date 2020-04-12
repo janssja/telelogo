e5c0bc588b45fba245512206b948c38031079972
Jackie Janssen <jackiejanssen@icloud.com>
2020-04-12 11:09:31 +0200

    first awesome commit

---
+++ telelogo/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from telelogo.users.models import User
+from telelogo.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
