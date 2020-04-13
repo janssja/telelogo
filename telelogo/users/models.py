81273200129c7ee6ee7893030ab224a55936bc3e
Jackie Janssen <jackiejanssen@icloud.com>
2020-04-13 14:25:38 +0200

    versiebeheer

--- telelogo/users/models.py
+++ telelogo/users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
@@ -8,7 +9,16 @@ class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = CharField(_("Gebruikersnaam"), blank=True, max_length=255)
+    is_student = models.BooleanField(_("Patiënt"), default=False)
+    is_teacher = models.BooleanField(_("Logopedist"), default=False)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        return self.user.username
--- telelogo/users/urls.py
+++ telelogo/users/urls.py
@@ -1,13 +1,18 @@
-from django.urls import path
+from classroom.views import classroom
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import include, path
 
-from telelogo.users.views import (
-    user_detail_view,
-    user_redirect_view,
-    user_update_view,
-)
+from telelogo.users.views import user_detail_view, user_redirect_view, user_update_view
 
 app_name = "users"
+
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('classroom.urls')),
+    path('accounts/signup/', classroom.SignUpView.as_view(), name='signup'),
+    path('accounts/login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
     path("<str:username>/", view=user_detail_view, name="detail"),
